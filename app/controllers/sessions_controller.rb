8ae96fc2a50fe22c72b199f46aaec26c94ae2357
Heejung Koo <heejung.koo@kellogg.northwestern.edu>
2025-03-03 05:04:07 +0000

    step 1 and 2 completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,31 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
-    flash["notice"] = "Nope."
+    # find user by email
+    @user = User.find_by({ "email" => params["email"] })
+    # if no user is found: redirect to `/login` with a `flash` message
+    if @user == nil
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    # if user exists: authenticate (i.e. check) their password
+    else
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    end
+  end
+
+  def destroy
+    # logout a user in the destroy action
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
     redirect_to "/login"
   end
+
+
 end
