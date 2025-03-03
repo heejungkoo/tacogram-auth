439b873b764410bceedb9390f54830449a25f62a
Heejung Koo <heejung.koo@kellogg.northwestern.edu>
2025-03-03 06:06:41 +0000

    step 3 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,12 @@ class PostsController < ApplicationController
   end
 
   def new
+    if User.find_by({ "id" => session["user_id"] }) != nil
+      @post = Post.new
+    else
+      flash["notice"] = "Please login to create a post"
+      redirect_to "/login"
+    end
   end
   
   def create
@@ -12,6 +18,7 @@ class PostsController < ApplicationController
     @post["body"] = params["body"]
     @post["image"] = params["image"]
     # TODO: assign logged-in user as user that created the post
+    @post["user_id"] = session["user_id"]
     @post.save
     redirect_to "/posts"
   end
